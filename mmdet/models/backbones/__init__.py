0fe2deb7e3518aa0e5f59f4788f84cd5a9adcd21
hj <3222726703@qq.com>
2020-12-23 22:03:11 +0800

    123

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -7,11 +7,9 @@ from .res2net import Res2Net
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
-from .resnet_at import ResNetAT
-from .resnext_at import ResNeXtAT
 from .sknetv2 import SKNetV2
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
-    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'ResNetAT', 'ResNeXtAT', 'SKNetV2'
+    'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'SKNetV2'
 ]
